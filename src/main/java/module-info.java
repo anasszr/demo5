6662ce7b37d122e805a7c1cc3d1270a5bb9292b8
anas <anasszrioual@gmail.com>
2024-09-27 13:55:54 -0400

    Commit #2 – Updated file

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,8 +1,1 @@
-module com.example.demo5 {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.example.demo5 to javafx.fxml;
-    exports com.example.demo5;
-}
+System.out.println("Goodbye");
